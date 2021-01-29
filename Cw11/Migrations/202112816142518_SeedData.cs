ca5d45cb3a9a7b2f9a45a8d849c3738cc12bdb75
s15935 <s15935@pjwstk.edu.pl>
2021-01-29 17:25:08 +0100

    Add files via upload

---
+++ Cw11/Migrations/202112816142518_SeedData.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cw11.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Doctor",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "KorolevAndri@gmail.com", "Andrii", "Korolew" },
+                    { 2, "AnastasiaMalanuk@gmail.com", "Anastasia", "Malanuk" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicament",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Lekarstwo na gorączkę", "Ibuprofen", "Syrop" },
+                    { 2, "Węgiel aktywowany", "Węgiel aktywowany", "Tabletka" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patient",
+                columns: new[] { "IdPatient", "Birthdate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 1, 28, 16, 13, 25, 18, DateTimeKind.Local).AddTicks(3753), "Sofia", "Gulman" },
+                    { 2, new DateTime(2021, 1, 28, 16, 13, 25, 18, DateTimeKind.Local).AddTicks(7227), "Marcin", "Hopinski" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescription",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 1, new DateTime(2021, 1, 28, 16, 13, 25, 18, DateTimeKind.Local).AddTicks(3358), new DateTime(2021, 1, 28, 16, 13, 25, 18, DateTimeKind.Local).AddTicks(3645), 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[] { 2, new DateTime(2021, 1, 28, 16, 13, 25, 18, DateTimeKind.Local).AddTicks(4352), new DateTime(2021, 1, 28, 16, 13, 25, 18, DateTimeKind.Local).AddTicks(4371), 2, 1 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 1, 1, "Szczegóły 1", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 2, 1, "Szczegóły 2", null });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[] { 1, 2, "Szczegóły 3", 2 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Medicament",
+                keyColumn: "IdMedicament",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Medicament",
+                keyColumn: "IdMedicament",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Doctor",
+                keyColumn: "IdDoctor",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctor",
+                keyColumn: "IdDoctor",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 1);
+        }
+    }
+}
